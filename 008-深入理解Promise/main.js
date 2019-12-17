ac5f15b6dc5731b4b2094ab70dc9cde0f0e4be64
unknown <snsart@163.com>
2019-12-17 15:07:07 +0800

    add

--- 008-深入理解Promise/main.js
+++ 008-深入理解Promise/main.js
@@ -1,15 +1,25 @@
-function Promise(callback){
-    
+function myPromise(callback){
+    this.callbacks=[];
+    let resolve=function(){
+        callbacks[0]();
+    }
+    let reject=function(){
+        callbacks[1]();
+    }
+    callback(resolve,reject);
 }
 
-Promise.prototype.then=function(callback){
-    callback();
+myPromise.prototype.then={
+    then:function(func1,func2){
+        this.callbacks.push(func1,func2);
+    }
 }
 
-let promise=new Promise((resolve,reject)=>{
+let promise=new myPromise(function(resolve,reject){
+    console.log("1245");
     setTimeout(()=>{
         resolve();
-    },1000)    
+    },3000)
 }).then(function(){
-
-})
+    console.log("shijiandao")
+});
